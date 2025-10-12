ab947fe81fb45c20568746f2c5b219ff59f2d846
kePREPRE <cuentaprincipalkeylor@gmail.com>
2025-10-12 10:26:07 -0600

    Base de Datos Primera Fase

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+      public function base()
+    {
+        return view("base");
+    }
+    
+}
---
+++ database/migrations/2025_10_12_153322_table_usuarios.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id('idUsuario');
+            $table->string('nombre', 100);
+            $table->string('correo', 150)->unique();
+            $table->string('contra', 255);
+            $table->boolean('activo')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
